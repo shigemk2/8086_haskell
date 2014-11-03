0b656a27dc62ac72d7568818722a8ede5dcacaec
shigemk2 <i.am.shige@gmail.com>
2014-11-03 14:17:28 +0900

    数値を2進数文字列に変換する

--- binint2str.hs
+++ binint2str.hs
@@ -3,13 +3,38 @@ module Main where
 import Test.HUnit
 import System.IO
 
-binIntToStr bin = ""
+binToInt '0' = 0
+binToInt '1' = 1
+
+binStrToInt bin = f (reverse bin)
+    where
+        f ""     = 0
+        f (x:xs) = (binToInt x) + 2 * (f xs)
+
+intToBin 0 = '0'
+intToBin 1 = '1'
+
+bin x
+    | x1 == 0   = x2
+    | otherwise = bin x1 ++ x2
+    where
+      x1 = x `div` 2
+      x2 = [intToBin $ x `mod` 2]
+
 tests = TestList
-    [ "binIntToStr 1" ~: binIntToStr 5 ~?= "101"
-    , "binIntToStr 2" ~: binIntToStr 25 ~?= "11001"
-    , "binIntToStr 3" ~: binIntToStr 31 ~?= "11111"
-    , "binIntToStr 3" ~: binIntToStr 50 ~?= "110010"
-    ]
+        [ "reverse"       ~: reverse     "11001"  ~?= "10011" 
+        , "binStrToInt 5" ~: binStrToInt "101"    ~?= 5
+        , "binStrToInt 25" ~: binStrToInt "11001"  ~?= 25
+        , "binStrToInt 31" ~: binStrToInt "11111"  ~?= 31
+        , "binStrToInt 350" ~: binStrToInt "110010" ~?= 50
+        , "div 1"         ~: 1 `div` 2     ~?= 0
+        , "bin 0" ~: bin 0 ~?= "0"
+        , "bin 1" ~: bin 1 ~?= "1"
+        , "bin 5" ~: bin 5 ~?= "101"
+        , "bin 25" ~: bin 25 ~?= "11001"
+        , "bin 31" ~: bin 31 ~?= "11111"
+        , "bin 50" ~: bin 50 ~?= "110010"
+        ]
 
 main = do
     runTestText (putTextToHandle stderr False) tests
