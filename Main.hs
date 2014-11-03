a09a012191e5ea99e466ea6f2445b25cca9f32d6
shigemk2 <i.am.shige@gmail.com>
2014-11-03 14:20:02 +0900

    16進数

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Test.HUnit
 import System.IO
+import Data.Char
 
 binToInt '0' = 0
 binToInt '1' = 1
@@ -21,6 +22,11 @@ bin x
       x1 = x `div` 2
       x2 = [intToBin $ x `mod` 2]
 
+hexStrToInt hex = f (reverse hex)
+    where
+        f ""     = 0
+        f (x:xs) = (digitToInt x) + 16 * (f xs)
+
 tests = TestList
         [ "reverse"       ~: reverse     "11001"  ~?= "10011" 
         , "binStrToInt 5" ~: binStrToInt "101"    ~?= 5
@@ -34,6 +40,9 @@ tests = TestList
         , "bin 25" ~: bin 25 ~?= "11001"
         , "bin 31" ~: bin 31 ~?= "11111"
         , "bin 50" ~: bin 50 ~?= "110010"
+        , "digitToInt" ~: digitToInt 'a' ~?= 10
+        , "hexStrToInt 1" ~: hexStrToInt "100"  ~?= 256
+        , "hexStrToInt 2" ~: hexStrToInt "ffff" ~?= 65535
         ]
 
 main = do
