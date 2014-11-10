2568998915954d67d710a66bdd23aaa01da58928
shigemk2 <i.am.shige@gmail.com>
2014-11-10 22:37:36 +0900

    テストの追加と修正

--- Main.hs
+++ Main.hs
@@ -156,8 +156,9 @@ testDisAsm = TestList
     , "c6-c7 mod=00,w=1 2" ~: disasm' "C700FF34" ~?= "mov word [bx+si],0x34ff"
     , "c6-c7 mod=01,w=1 1" ~: disasm' "C740123456" ~?= "mov word [bx+si+0x12],0x5634"
     , "c6-c7 mod=01,w=1 2" ~: disasm' "C740FF1234" ~?= "mov word [bx+si-0x1],0x3412"
-    , "c6-c7 mod=01,w=1 2" ~: disasm' "C740FF3456" ~?= "mov word [bx+si-0x1],0x5634"
+    , "c6-c7 mod=01,w=1 3" ~: disasm' "C740FF3456" ~?= "mov word [bx+si-0x1],0x5634"
     , "c6-c7 mod=10,w=1 1" ~: disasm' "C78012345678" ~?= "mov word [bx+si+0x3412],0x7856"
+    , "c6-c7 mod=10,w=1 2" ~: disasm' "C780FFFFFFFF" ~?= "mov word [bx+si-0x1],0xffff"
     ]
 
 main = do
