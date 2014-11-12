d724d779e229a97782700c96f4374f85b0785d7a
shigemk2 <i.am.shige@gmail.com>
2014-11-12 13:26:16 +0900

    Immediate to Register/Memory のmod=11に対応した

--- DisAsm.hs
+++ DisAsm.hs
@@ -13,7 +13,7 @@ disasmB (1,0,0,0,1,0,d,w) xs
     | d == 0    = "mov " ++ rm  ++ "," ++ reg
     | otherwise = "mov " ++ reg ++ "," ++ rm
     where
-        ((len, rm), r) = modrm w xs
+        ((len, mod, rm), r) = modrm w xs
         reg = regs !! w !! r
 
 disasmB (1,0,1,1,w,r,e,g) xs =
@@ -23,10 +23,12 @@ disasmB (1,0,1,1,w,r,e,g) xs =
         imm = "0x" ++ hex (fromLE (w + 1) xs)
 
 disasmB (1,1,0,0,0,1,1,w) xs
+    | mod == 3  = "mov " ++ rm  ++ "," ++ imm
     | w == 0    = "mov byte " ++ rm  ++ "," ++ imm
-    | otherwise = "mov word " ++ rm  ++ "," ++ imm
+    | w == 1    = "mov word " ++ rm  ++ "," ++ imm
+    | otherwise = "mov " ++ rm  ++ "," ++ imm
     where
-        ((len, rm), r) = modrm w xs
+        ((len, mod, rm), r) = modrm w xs
         imm = "0x" ++ hex (fromLE (w + 1) $ drop (len + 1) xs)
 
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
@@ -36,15 +38,15 @@ modrm w (x:xs) = (f mode rm, reg)
         mode =  x `shiftR` 6
         reg  = (x `shiftR` 3) .&. 7
         rm   =  x             .&. 7
-        f 0 6  = (2, "[0x" ++ hex (fromLE 2 xs) ++ "]")
-        f 0 rm = (0, "[" ++ regad !! rm ++ "]")
-        f 1 rm = (1, "[" ++ regad !! rm ++ disp ++ "]")
+        f 0 6  = (2, 0, "[0x" ++ hex (fromLE 2 xs) ++ "]")
+        f 0 rm = (0, 0, "[" ++ regad !! rm ++ "]")
+        f 1 rm = (1, 1, "[" ++ regad !! rm ++ disp ++ "]")
             where
                 disp = disp8 (xs !! 0)
-        f 2 rm = (2, "[" ++ regad !! rm ++ disp ++ "]")
+        f 2 rm = (2, 2, "[" ++ regad !! rm ++ disp ++ "]")
             where
                 disp = disp16 (fromLE 2 xs)
-        f 3 rm = (0, regs !! w !! rm)
+        f 3 rm = (0, 3, regs !! w !! rm)
 
 reg16 = ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"]
 reg8  = ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"]
