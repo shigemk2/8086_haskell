24d3f34e5ee6e86be3f1eb6d3fe3baa5bceec03f
shigemk2 <i.am.shige@gmail.com>
2014-12-08 23:00:35 +0900

    ror命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -472,6 +472,13 @@ disasmB (1,1,0,1,0,0,v,w) xs
     where
         (len, rm, r) = modrm True w xs
 
+-- ror
+disasmB (1,1,0,1,0,0,v,w) xs
+    | r == 1 && v == 0 = (len + 1, "ror " ++ rm ++ ",1")
+    | r == 1 && v == 1 = (len + 1, "ror " ++ rm ++ ",cl")
+    where
+        (len, rm, r) = modrm True w xs
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
