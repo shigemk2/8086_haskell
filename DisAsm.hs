e1d74ef83beff5ecda6851de11d089e71d94a30f
shigemk2 <i.am.shige@gmail.com>
2014-12-08 09:24:46 +0900

    shl/sal命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -444,6 +444,13 @@ disasmB (1,1,1,1,0,1,1,w) xs
     where
         (len, rm, r) = modrm True w xs
 
+-- shl/sal
+disasmB (1,1,0,1,0,0,v,w) xs
+    | v == 0 = (len + 1, "shl " ++ rm ++ ",1")
+    | v == 1 = (len + 1, "shl " ++ rm ++ ",cl")
+    where
+        (len, rm, r) = modrm True w xs
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
