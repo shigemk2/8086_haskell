4f0f4f84fba090ca9d9f0e59b23100044a917539
shigemk2 <i.am.shige@gmail.com>
2014-12-09 13:44:40 +0900

    and命令 Reg./Memory and Register to Either 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -493,6 +493,15 @@ disasmB (1,1,0,1,0,0,v,w) xs
     where
         (len, rm, r) = modrm True w xs
 
+-- and
+-- Reg./Memory and Register to Either
+disasmB (0,0,1,0,0,0,d,w) xs
+    | d == 0    = (1 + len, "and " ++ rm  ++ "," ++ reg)
+    | otherwise = (1 + len, "and " ++ reg ++ "," ++ rm)
+    where
+        (len, rm, r) = modrm False w xs
+        reg = regs !! w !! r
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
