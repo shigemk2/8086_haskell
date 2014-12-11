2aca48374d3108f38e8f7052cca0275b7c8ab4ae
shigemk2 <i.am.shige@gmail.com>
2014-12-11 20:40:08 +0900

    xor命令 Immediate to Register/Memory 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -573,6 +573,14 @@ disasmB (0,0,1,1,0,0,d,w) xs
         (len, rm, r) = modrm False w xs
         reg = regs !! w !! r
 
+-- Immediate to Register/Memory
+disasmB (1,0,0,0,0,0,0,w) xs
+    | w == 0 && r == 6 = (2 + len, "xor " ++ rm ++ "," ++ imm)
+    | w == 1 && r == 6 = (2 + 1 + len, "xor " ++ rm ++ "," ++ imm)
+    where
+        (len, rm, r) = modrm True w xs
+        imm = "0x" ++ hex (fromLE (w + 1) (drop len xs))
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
