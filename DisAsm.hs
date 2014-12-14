42a62c3eb17feebf61f382c4a2c5cbf179ef4c81
shigemk2 <i.am.shige@gmail.com>
2014-12-14 18:01:44 +0900

    ret命令 Intersegment Adding Immediate to SP 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -696,6 +696,12 @@ disasmB _ (1,1,0,0,0,0,1,0) xs =
 disasmB _ (1,1,0,0,1,0,1,1) xs =
     (1, "retf")
 
+-- Intersegment Adding Immediate to SP
+disasmB _ (1,1,0,0,1,0,1,0) xs =
+    (3, "retf " ++ imm)
+    where
+        imm = "0x" ++ hex (fromLE 2 xs)
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
