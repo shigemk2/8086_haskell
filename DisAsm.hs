4600e0705a149ce5ad4ea8f512d2d3fb6288df9e
shigemk2 <i.am.shige@gmail.com>
2014-12-15 20:50:01 +0900

    jnb/jae命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -779,6 +779,13 @@ disasmB ip (0,1,1,1,1,1,1,1) xs =
         len = 2
         imm = "0x" ++ hex (fromLE 1 xs + ip + len)
 
+-- jnb/jae
+disasmB ip (0,1,1,1,0,0,1,1) xs =
+    (len, "jnc " ++ imm)
+    where
+        len = 2
+        imm = "0x" ++ hex (fromLE 1 xs + ip + len)
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
