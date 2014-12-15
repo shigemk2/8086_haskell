138086e4f8478370ae973cbe49c1863b539c46ed
shigemk2 <i.am.shige@gmail.com>
2014-12-15 14:46:26 +0900

    jnl/jge命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -765,6 +765,13 @@ disasmB ip (0,1,1,1,0,1,0,1) xs =
         len = 2
         imm = "0x" ++ hex (fromLE 1 xs + ip + len)
 
+-- jnl/jge
+disasmB ip (0,1,1,1,1,1,0,1) xs =
+    (len, "jnl " ++ imm)
+    where
+        len = 2
+        imm = "0x" ++ hex (fromLE 1 xs + ip + len)
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
