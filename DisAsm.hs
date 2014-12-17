ba58d5da4b8beff7c995b44cbeab4b9381edaf6f
shigemk2 <i.am.shige@gmail.com>
2014-12-17 09:47:24 +0900

    jcxz命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -835,6 +835,13 @@ disasmB ip (1,1,1,0,0,0,0,0) xs =
         len = 2
         imm = "0x" ++ hex (fromLE 1 xs + ip + len)
 
+-- jcxz
+disasmB ip (1,1,1,0,0,0,1,1) xs =
+    (len, "jcxz " ++ imm)
+    where
+        len = 2
+        imm = "0x" ++ hex (fromLE 1 xs + ip + len)
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
