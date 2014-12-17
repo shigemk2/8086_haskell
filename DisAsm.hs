60c11b00c025458a1f977f42740d2a59dd645aa1
shigemk2 <i.am.shige@gmail.com>
2014-12-17 23:37:54 +0900

    wait命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -893,6 +893,10 @@ disasmB _ (1,1,1,1,1,0,1,1) xs =
 disasmB _ (1,1,1,1,0,1,0,0) xs =
     (1, "hlt")
 
+-- wait
+disasmB _ (1,0,0,1,1,0,1,1) xs =
+    (1, "wait")
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
