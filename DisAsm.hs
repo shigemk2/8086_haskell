9a74a178296f893ea25ff594d09cb811af567545
shigemk2 <i.am.shige@gmail.com>
2014-12-17 23:35:35 +0900

    hlt命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -889,6 +889,10 @@ disasmB _ (1,1,1,1,1,0,1,0) xs =
 disasmB _ (1,1,1,1,1,0,1,1) xs =
     (1, "sti")
 
+-- hlt
+disasmB _ (1,1,1,1,0,1,0,0) xs =
+    (1, "hlt")
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
