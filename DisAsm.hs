aaecb46bfc82702a971b1123dc5a04ccd9691e7d
shigemk2 <i.am.shige@gmail.com>
2014-12-17 09:39:06 +0900

    loopnz/loopne命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -828,6 +828,13 @@ disasmB ip (1,1,1,0,0,0,0,1) xs =
         len = 2
         imm = "0x" ++ hex (fromLE 1 xs + ip + len)
 
+-- loopnz/loopne
+disasmB ip (1,1,1,0,0,0,0,0) xs =
+    (len, "loopne " ++ imm)
+    where
+        len = 2
+        imm = "0x" ++ hex (fromLE 1 xs + ip + len)
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
