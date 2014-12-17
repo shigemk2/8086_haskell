d7c5bdea2d91bf97032248d8d35a263c0f897412
shigemk2 <i.am.shige@gmail.com>
2014-12-17 19:33:32 +0900

    clc命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -861,6 +861,10 @@ disasmB _ (1,1,0,0,1,1,1,0) xs =
 disasmB _ (1,1,0,0,1,1,1,1) xs =
     (1, "iretw")
 
+-- clc
+disasmB _ (1,1,1,1,1,0,0,0) xs =
+    (1, "clc")
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
