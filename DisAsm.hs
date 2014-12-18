f2106e3c60cc367b6c4af435e2a403b7bd83a8f7
shigemk2 <i.am.shige@gmail.com>
2014-12-18 14:08:43 +0900

    lock命令 実装

--- DisAsm.hs
+++ DisAsm.hs
@@ -897,6 +897,10 @@ disasmB _ (1,1,1,1,0,1,0,0) xs =
 disasmB _ (1,0,0,1,1,0,1,1) xs =
     (1, "wait")
 
+-- lock
+disasmB _ (1,1,1,1,0,0,0,0) xs =
+    (1, "lock")
+
 regad = ["bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"]
 
 modrm prefix w (x:xs) = (len, s, reg)
